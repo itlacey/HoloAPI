67e6e70ea6260db14bd743485d40c3abdc2991a0
itlacey <ikelacey@gmail.com>
2024-04-29 09:22:02 -0400

    Testing HoloAPI

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from .models.items import Item
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/love-note")
+async def root():
+    return {"message": "This is from the HoloAPI"}
